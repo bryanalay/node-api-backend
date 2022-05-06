54e93b0f0a2193bc62f2876934f331f6e2146d15
Bryan alay <bryanalmi27@hotmail.com>
2022-05-05 21:22:38 -0500

    sera que vale?

--- mongo.js
+++ mongo.js
@@ -5,7 +5,8 @@ const conectionString = process.env.MONGO_DB_URI
 // conexion a mongodb
 mongoose.connect(conectionString, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000
 })
   .then(() => {
     console.log('Datebase Connected')
