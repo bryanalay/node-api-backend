adf550fc71886fd167d8401735079547183579df
Bryan alay <bryanalmi27@hotmail.com>
2022-05-05 21:43:21 -0500

    pbb solution

--- mongo.js
+++ mongo.js
@@ -5,8 +5,7 @@ const conectionString = process.env.MONGO_DB_URI
 // conexion a mongodb
 mongoose.connect(conectionString, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 5000
+  useUnifiedTopology: true
 })
   .then(() => {
     console.log('Datebase Connected')
